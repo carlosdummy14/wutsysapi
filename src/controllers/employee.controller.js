4e1c48e5465df209476756859267101ba1d00778
carlos <carlosdummy14@gmail.com>
2022-05-05 23:37:11 -0500

    work: employee endpoint

--- src/controllers/employee.controller.js
+++ src/controllers/employee.controller.js
@@ -4,56 +4,94 @@ const DEFAULT_AVATAR =
   'https://www.gravatar.com/avatar/c21f969b5f03d33d43e04f8f136e7682?d=robohash&s=200'
 
 const getAll = async (req, res) => {
-  const employees = await Employee.find({})
+  try {
+    const employees = await Employee.find({})
 
-  res
-    .status(200)
-    .json({ msg: 'employees', data: employees, count: employees.length })
+    res
+      .status(200)
+      .json({ msg: 'employees', data: employees, count: employees.length })
+  } catch (error) {
+    res.status(500).json({ msg: error })
+  }
 }
 
 const createOne = async (req, res) => {
   const { internalId, name, position, area, avatar } = req.body
 
   if (!internalId || !name || !position || !area) {
-    res.status(403).json({ msg: 'Please provide a valid employee' })
+    res.status(400).json({ msg: 'Please provide a valid employee' })
     return
   }
 
   const existEmployee = await Employee.findOne({ internalId })
   if (existEmployee) {
-    res.status(403).json({ msg: 'Employee already exist' })
+    res.status(400).json({ msg: 'Employee already exist' })
     return
   }
 
   const data = {
-    ...req.body,
+    internalId,
+    name,
+    position,
+    area,
     avatar: avatar || DEFAULT_AVATAR,
   }
 
-  const newEmployee = await Employee.create(data)
-
-  res.status(201).json({ msg: 'employee created', data: newEmployee })
+  try {
+    const newEmployee = await Employee.create(data)
+    res.status(201).json({ msg: 'employee created', data: newEmployee })
+  } catch (error) {
+    res.status(500).json({ msg: error })
+  }
 }
 
 const getOne = async (req, res) => {
   const { id: employeeId } = req.params
 
   if (!employeeId) {
-    res.status(403).json({ msg: 'Provide a valid employee' })
+    res.status(400).json({ msg: 'Provide a valid employee' })
     return
   }
 
-  const employee = await Employee.findById(employeeId)
-  if (!employee) {
-    res.status(403).json({ msg: 'Employee did not exist' })
-    return
-  }
+  try {
+    const employee = await Employee.findById(employeeId)
+    if (!employee) {
+      res.status(400).json({ msg: 'Employee did not exist' })
+      return
+    }
 
-  res.status(200).json({ msg: 'ok', data: employee })
+    res.status(200).json({ msg: 'ok', data: employee })
+  } catch (error) {
+    res.status(500).json({ msg: error })
+  }
 }
 
 const updateOne = async (req, res) => {
-  res.status(200).json({ msg: 'update one employee' })
+  const { name, position, area, avatar } = req.body
+  const { id: employeeId } = req.params
+
+  if (!employeeId || !name || !position || !area) {
+    res.status(400).json({ msg: 'Please provide a valid employee' })
+    return
+  }
+
+  try {
+    const employeeToUpdate = await Employee.findById(employeeId)
+    if (!employeeToUpdate) {
+      res.status(400).json({ msg: 'Employee did not exist' })
+      return
+    }
+
+    employeeToUpdate.name = name
+    employeeToUpdate.position = position
+    employeeToUpdate.area = area
+    employeeToUpdate.avatar = avatar || DEFAULT_AVATAR
+
+    await employeeToUpdate.save()
+    res.status(200).json({ msg: 'employee updated', data: employeeToUpdate })
+  } catch (error) {
+    res.status(500).json({ msg: error })
+  }
 }
 
 const deleteOne = async (req, res) => {
@@ -64,15 +102,19 @@ const deleteOne = async (req, res) => {
     return
   }
 
-  const existEmployee = await Employee.findById(employeeId)
-  if (!existEmployee) {
-    res.status(403).json({ msg: 'Employee did not exist' })
-    return
+  try {
+    const employeeToDelete = await Employee.findById(employeeId)
+    if (!employeeToDelete) {
+      res.status(403).json({ msg: 'Employee did not exist' })
+      return
+    }
+
+    await employeeToDelete.remove()
+
+    res.status(200).json({ msg: 'deleted' })
+  } catch (error) {
+    res.status(500).json({ msg: error })
   }
-
-  await Employee.findOneAndRemove({ _id: employeeId })
-
-  res.status(200).json({ msg: 'deleted' })
 }
 
 module.exports = { getAll, createOne, getOne, updateOne, deleteOne }
--- src/models/employee.model.js
+++ src/models/employee.model.js
@@ -5,23 +5,32 @@ const EmployeeSchema = new Schema(
     internalId: {
       type: String,
       required: true,
+      trim: true,
       unique: true,
+      maxlength: [20, 'internalId too large max size 20'],
     },
     name: {
       type: String,
+      trim: true,
       required: true,
+      maxlength: [50, 'name too large max size 50'],
     },
     position: {
       type: String,
+      trim: true,
       required: true,
+      maxlength: [20, 'position too large max size 20'],
     },
     area: {
       type: String,
+      trim: true,
       required: true,
+      maxlength: [20, 'area too large max size 20'],
     },
     avatar: {
       type: String,
       required: true,
+      trim: true,
       default: '',
     },
   },
