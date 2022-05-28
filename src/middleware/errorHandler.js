869f445d8502e17dcf5a71736b4fa9812a55d5d6
carlos <carlosdummy14@gmail.com>
2022-05-27 23:59:52 -0500

    fix: check ObjectId errors

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -6,6 +6,10 @@ const errorHandler = (err, req, res, next) => {
     return res.status(err.statusCode).json({ msg: err.message })
   }
 
+  if (err.kind === 'ObjectId') {
+    return res.status(StatusCodes.BAD_REQUEST).json({ msg: `Id:${err.value} is not valid` })
+  }
+
   return res
     .status(StatusCodes.INTERNAL_SERVER_ERROR)
     .json({ msg: 'Ups!! Something was wrong...', err: err.message })
